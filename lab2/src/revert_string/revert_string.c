84dcbd32adcb79f3495fad6716bb560c75698d30
atlantaboy69 <ananievroma69@gmail.com>
2026-09-05 15:01:31 +0000

    выполнена 2 лаба

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,21 @@
 #include "revert_string.h"
+#include <string.h>
 
-void RevertString(char *str)
-{
-	// your code here
+void RevertString(char *str) {
+    if (str == NULL) return; // Защита от передачи пустого указателя
+
+    int length = strlen(str);
+    int left = 0;
+    int right = length - 1;
+
+    // Двигаемся с двух сторон к центру и меняем символы местами
+    while (left < right) {
+        char temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+
+        left++;
+        right--;
+    }
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,7 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char temp = *left;  // Сохраняем значение, на которое указывает left
+    *left = *right;     // Записываем значение из right в адрес left
+    *right = temp;   
 }
